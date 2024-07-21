12943c6b47464f914e36249a9a1bf6811686981d
AvavaAva <995311855@qq.com>
2024-07-22 00:35:20 +0900

    modify pretrain

--- src/train_flow/train_ml_models.py
+++ src/train_flow/train_ml_models.py
@@ -10,7 +10,7 @@ from src.pkts import retrain_modules
 
 def train_single_model_sampling(feats, labels,
                                 x_train, x_test, y_train, y_test,
-                                model, sampling, trials, one_hot=False,
+                                model, trials, one_hot=False,
                                 metric='f1-macro', class_weights=None, rif=False):
     """
     Pretrain single model based on specific sampling and one-hot choice.
@@ -23,7 +23,6 @@ def train_single_model_sampling(feats, labels,
     :param y_train: for retrain, y
     :param y_test: for evaluation, y
     :param model: MODEL to train
-    :param sampling: Sampling tech
     :param trials: running trials for hyper-param searching
     :param one_hot: Whether one-hot used, default false
     :param metric: evaluation metric, default f1-macro
@@ -31,16 +30,16 @@ def train_single_model_sampling(feats, labels,
     :return: training results (for evaluating model performance!)
     """
     # Pretrain for hyper-params tuning
-    best_params, _, result_dir = param_search_by_optuna(feats, labels, sampling, study_name=model,
+    best_params, _, result_dir = param_search_by_optuna(feats, labels, study_name=model,
                                                         metric=metric, class_weights=class_weights,
                                                         n_trials=trials, one_hot=one_hot)
 
     # Retrain for performance evaluation
-    rif_str = 'with_rif' if rif else 'no_rif'
-    un_samp = 'None'
-    if rif:
-        un_samp = 'TomekLinks'  # add: Never use None, use rus and tl! (data prepared)
-        x_train, y_train, x_test = retrain_modules.prepare_rif_setting(x_train, y_train, x_test)
-    use_sampling = un_samp if un_samp != 'None' else sampling
-    retrain_modules.retrain(model, best_params, x_train, y_train, x_test, y_test, sampling=use_sampling,
-                            class_weights=class_weights, save_dir=result_dir, rifed=rif_str)
+    # rif_str = 'with_rif' if rif else 'no_rif'
+    # un_samp = 'None'
+    # if rif:
+    #     un_samp = 'TomekLinks'  # add: Never use None, use rus and tl! (data prepared)
+    #     x_train, y_train, x_test = retrain_modules.prepare_rif_setting(x_train, y_train, x_test)
+    # use_sampling = un_samp if un_samp != 'None' else sampling
+    # retrain_modules.retrain(model, best_params, x_train, y_train, x_test, y_test, sampling=use_sampling,
+    #                         class_weights=class_weights, save_dir=result_dir, rifed=rif_str)
