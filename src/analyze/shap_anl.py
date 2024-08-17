117ac0f95d1e7f32839046325f086ea16d83a143
AvavaAva <995311855@qq.com>
2024-08-17 23:35:21 +0900

    Delete TODO

--- src/analyze/shap_anl.py
+++ src/analyze/shap_anl.py
@@ -65,14 +65,3 @@ if __name__ == "__main__":
         plt.savefig(os.path.join(heatmap_path, f"SHAP_heatmap_for_{inury_level[i]}.png"), dpi=300, bbox_inches='tight')
         plt.gca().tick_params(axis='y', labelsize=7)
         plt.show()
-
-    # TODO: change shap cases!
-
-"""
-f(x) 曲线：
-
-图上方的 f(x) 曲线表示模型的输出值（预测值）的变化情况。对于每个样本（或实例），f(x) 代表模型的预测值。当你处理的是一个回归任务时，f(x) 可能是一个连续值；对于分类任务，它可能是某个类别的预测概率。
-右方的柱状图：
-
-右侧的黑色柱状图表示每个特征在所有样本中的总体重要性。这些柱状图显示了每个特征在多个样本中 SHAP 值的总和或平均值。柱状图越长，表示该特征对模型输出的影响越大。
-"""
